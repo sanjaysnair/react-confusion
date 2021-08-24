457bf2fba22d7dfda9e261e554af997e52c5a52c
Sanjay Nair <getsanjaysnair@gmail.com>
2021-08-24 08:03:31 +0530

    React redux form revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Footer from "./FooterComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { addComment, fetchDishes } from "../redux/ActionCreators";
+import { actions } from "react-redux-form";
 
 const mapStateToProps = (state) => {
     return {
@@ -24,6 +25,9 @@ const mapDispatchToProps = (dispatch) => ({
         dispatch(addComment(dishId, rating, author, comment)),
     fetchDishes: () => {
         dispatch(fetchDishes()); // calling the thunk fetchDishes()
+    },
+    resetFeedbackForm: () => {
+        dispatch(actions.reset("feedback"));
     },
 });
 
@@ -90,7 +94,15 @@ class Main extends Component {
                         component={() => <Menu dishes={this.props.dishes} />}
                     />
                     <Route path="/menu/:dishId" component={DishWithId} />
-                    <Route exact path="/contactus" component={Contact} />
+                    <Route
+                        exact
+                        path="/contactus"
+                        component={() => (
+                            <Contact
+                                resetFeedbackForm={this.props.resetFeedbackForm}
+                            />
+                        )}
+                    />
                     <Route
                         exact
                         path="/aboutus"
